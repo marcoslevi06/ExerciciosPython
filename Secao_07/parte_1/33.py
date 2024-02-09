574f4dc694b8464c159317adec3fbd382b7cb7c3
Marcos Levi <levi13.marcos@gmail.com>
2024-02-09 15:27:49 -0300

    ex 33

--- Secao_07/parte_1/33.py
+++ Secao_07/parte_1/33.py
@@ -1,22 +1,20 @@
 import random
 
+# v = [0, 1, 2, 0]
 v = []
+temp = 0
 
-for i in range(0, 15):
-    v.append(random.randint(0, 10))
-
-print(v)
-print(sorted(v))
-
-for i in v:
-    for j in range(0, len(v)):
-
-        if i <= v[j]:
-            v[j] = i
-            pos2 = v[j]+ 1
-
-
-            v[pos1] = v[j]
-            break
+for _ in range(0, 15):
+    v.append(random.randint(0, 5))
 
 print(v)
+
+for i in range(0, len(v)):
+    if v[i] == 0:
+        for j in range(i, len(v) - 1):
+            # temp = v[j+1]
+            # v[j +1] = v[i]
+            # v[i] = temp
+            v[j], v[j + 1] = v[j + 1], v[j]
+
+print("Resultado final da lista:", v)
